e5e866b58fbd2484268479c979f095843c1fb8f5
larasmey <69838304+laras61@users.noreply.github.com>
2024-10-04 16:03:27 +0800

    Add files via upload

---
+++ texteditor/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        txteditor = New TextBox()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 29)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OpenToolStripMenuItem, SaveToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(46, 25)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.O
+        OpenToolStripMenuItem.Size = New Size(180, 26)
+        OpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.S
+        SaveToolStripMenuItem.Size = New Size(180, 26)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(180, 26)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' txteditor
+        ' 
+        txteditor.Dock = DockStyle.Fill
+        txteditor.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txteditor.Location = New Point(0, 29)
+        txteditor.Multiline = True
+        txteditor.Name = "txteditor"
+        txteditor.ScrollBars = ScrollBars.Vertical
+        txteditor.Size = New Size(800, 421)
+        txteditor.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txteditor)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "My Text Editor"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents txteditor As TextBox
+
+End Class
---
+++ texteditor/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+Imports System.Windows.Forms ' pastikan namespace Forms diimport
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private savetext As SaveFileDialog 'for saving text
+    Private opentext As OpenFileDialog 'for opening text 
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Try
+            'set properties of the savefiledialog
+            savetext = New SaveFileDialog
+            savetext.FileName = "Untitled" 'set this as default title of every new text file
+            'set the allowed file type to be saved
+            'only text file is allowed 
+            savetext.Filter = "Text file only (*.txt)|*.txt"
+
+            With savetext
+                .AddExtension = True
+                .CheckPathExists = True
+                .CheckFileExists = False ' ubah menjadi False karena saat save, file belum ada
+                .OverwritePrompt = True
+                .ValidateNames = True
+                .DefaultExt = "txt"
+                .ShowHelp = True
+
+                'save the text file
+                If savetext.ShowDialog() = DialogResult.OK Then
+                    Try
+                        My.Computer.FileSystem.WriteAllText(.FileName, txteditor.Text, False)
+                        'get and set file name on the app title window
+                        Me.Text = "My Text Editor " & .FileName
+                    Catch fileexception As Exception
+                        Throw fileexception
+                    End Try
+                End If
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Try
+            'set properties of the openfiledialog
+            opentext = New OpenFileDialog
+            opentext.FileName = ""
+            'set the file type to be opened
+            'the file type is only text file
+            opentext.Filter = "Text file only (*.txt)|*.txt"
+
+            With opentext
+                .DefaultExt = "txt"
+                .Title = "Open a text file"
+                .CheckFileExists = True
+                .Multiselect = False
+
+                If opentext.ShowDialog() = DialogResult.OK Then
+                    Try
+                        'open text file in text box of the application
+                        txteditor.Text = My.Computer.FileSystem.ReadAllText(.FileName)
+                        'get and set the title of the opened file on app window
+                        Me.Text = "My Text Editor " & .FileName
+                    Catch fileexception As Exception
+                        Throw fileexception
+                    End Try
+                End If
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
